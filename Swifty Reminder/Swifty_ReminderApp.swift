697ee95445e5375fb07b78314702b1caa9e60da3
Aman <amanbind007@gmail.com>
2023-09-15 17:29:51 +0530

    Initial Commit

---
+++ Swifty Reminder/Swifty_ReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swifty_ReminderApp.swift
+//  Swifty Reminder
+//
+//  Created by Aman Bind on 15/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Swifty_ReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
